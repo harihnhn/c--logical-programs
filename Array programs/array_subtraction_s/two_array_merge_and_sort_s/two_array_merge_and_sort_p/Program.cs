4cda74b2d47db2a7a8f0db291815dde00eb4f66a
Hariharan <110807460+harihnhn@users.noreply.github.com>
2022-12-02 15:31:58 +0530

    msg

---
+++ Array programs/array_subtraction_s/two_array_merge_and_sort_s/two_array_merge_and_sort_p/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace two_array_merge_and_sort_p
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j=0;
+            bool flag = true;
+            int[] arr1, arr2, arr3;
+            arr1 = new int[] { 0, 2, 4, 6, 8 };
+            arr2 = new int[] { 1, 3, 5, 7, 9 };
+            arr3 = new int[arr1.Length + arr2.Length];
+            
+            //merge two array
+            for(i = 0; i<arr1.Length; i++)            
+                arr3[j++] = arr1[i]; 
+            
+            for (i = 0; i < arr2.Length; i++)
+                arr3[j++] = arr2[i];
+
+            //sort array
+            while(flag)
+            {
+                flag = false;
+                for (i = 0; i < arr3.Length - 1; i++)
+                {
+                    if (arr3[i] > arr3[i + 1])
+                    {
+                        //arr3[i] = arr3[i + 1] + arr3[i];
+                        //arr3[i + 1] = arr3[i] - arr3[i + 1];
+                        //arr3[i] = arr3[i] - arr3[i + 1];
+
+                        arr3[i] = (arr3[i] * arr3[i + 1]) / (arr3[i + 1] = arr3[i]);
+                        flag = true;
+                    }
+                }
+            }
+
+            //print output
+            Console.Write("C = [ ");
+            for (i = 0; i< arr3.Length;i++)
+            {
+                Console.Write(arr3[i]+ " ");
+            }
+            Console.Write("]");
+            Console.Read();
+
+            
+        }
+    }
+}
---
+++ print_data_type_of_given_data_s/print_data_type_of_given_data_p/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_data_type_of_given_data_p
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = Console.ReadLine();
+            int d1 = int.Parse(data);
+            float d2 = float.Parse(data);
+
+            if ((d2 / d1) > 1) Console.WriteLine("float");
+            else if(d1-d1 == 0) Console.WriteLine("int");
+            else if(data.Length == 1) Console.WriteLine("char");
+            else Console.WriteLine("string");
+
+            Console.Read();
+        }
+    }
+}
